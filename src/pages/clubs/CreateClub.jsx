04023758fb9150b2586ae94e8ac1cda404ba45f2
Elaine <omwengaelaine@gmail.com>
2025-07-28 11:03:45 +0300

    change file path

--- src/pages/clubs/CreateClub.jsx
+++ src/pages/clubs/CreateClub.jsx
@@ -1,6 +1,6 @@
 "use client"
 import { Navigate } from "react-router-dom"
-import { useAuth } from "../context/AuthContext"
+import { useAuth } from "../../context/AuthContext"
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, loading } = useAuth()
